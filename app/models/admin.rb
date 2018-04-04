1a6c0765412d9f709f0e316093beb4cb090b7e23
Willian Teophilo <wteophilo@gmail.com>
2018-04-03 21:09:26 -0300

    corrigindo problema dos enums, apenas funcionam números

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,10 +1,14 @@
 class Admin < ActiveRecord::Base
+  #Constantes
+  ROLES  = {:full_access => 0,:restricted_access =>1}
+  #enuns
+  enum role: ROLES
+  scope :with_full_access, -> {where(role: ROLES[:full_access])}
+  scope :with_restricted_access, -> {where(role: ROLES[:restricted_access])}
 
-  enum role: [:full_access,:restricted_access]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  scope :with_full_access, -> {where(role: 'full_access')}
-end
+  end
