a6f0875dba6519fd7e20b745969dfcabbe630f1d
Willian Teophilo <wteophilo@gmail.com>
2018-04-03 21:31:27 -0300

    aula 38: hashes

--- app/controllers/backoffice/admins_controller.rb
+++ app/controllers/backoffice/admins_controller.rb
@@ -21,13 +21,6 @@ class Backoffice::AdminsController < BackofficeController
   end
 
   def update
-    passwd = params[:admin][:password]
-    passwd_confirmation = params[:admin][:password_confirmation]
-  	
-    if passwd.blank? && passwd_confirmation.blank?
-      params[:admin].delete(:password)
-      params[:admin].delete(:password_confirmation)
-    end
     
     if @admin.update(params_admin)
   		redirect_to backoffice_admins_path,notice: "Salvo com sucesso!"
@@ -49,9 +42,16 @@ class Backoffice::AdminsController < BackofficeController
 
   def set_admin
   	@admin = Admin.find(params[:id])
-  end
+  end 
 
   def params_admin
+    passwd = params[:admin][:password]
+    passwd_confirmation = params[:admin][:password_confirmation]
+    
+    if passwd.blank? && passwd_confirmation.blank?
+      params[:admin].except!(:password,:password_confirmation)
+    end
+    
   	params.require(:admin).permit(:name,:email,:password,:password_confirmation)
   end
 end
