96de311df987cdf322d46c7888e2b301cff74887
Willian Teophilo <wteophilo@gmail.com>
2018-04-14 17:59:36 -0300

    Validação tipo acesso usuário

--- app/policies/admin_policy.rb
+++ app/policies/admin_policy.rb
@@ -1,6 +1,18 @@
 class AdminPolicy < ApplicationPolicy
   def new?
-	user.full_access?		
+	 user.full_access?		
+  end
+
+  def edit?
+   user.full_access?    
+  end
+
+  def permitted_attributes  
+    if user.full_access?
+      [:name,:email,:role,:password,:password_confirmation]
+    else
+       [:name,:email,:password,:password_confirmation]
+    end
   end
 
   class Scope < Scope
